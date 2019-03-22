7e6dcabd240644f7f18b771691adb7b2d153b837
marielosagui <00078913@uca.edu.sv>
2019-03-22 07:11:43 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/labo3_pdm_00078913/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.labo3_pdm_00078913;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
